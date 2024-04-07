1683748d4d58a53502e6b1a38e96ef2c492e758a
FridrichMethod <zhaoyangli@stanford.edu>
2024-04-08 02:16:06 +0800

    mmgbsa bugs fixed

--- MMGBSA/mmgbsa_genpv.sh
+++ MMGBSA/mmgbsa_genpv.sh
@@ -9,18 +9,18 @@ for dir in ./*/; do
 
     rec_path="${dir}/${dir##*/}.maegz"
     if [ ! -f "$rec_path" ]; then
-        echo "No receptor file found in $dir."
+        echo "No receptor file found in $dir"
         continue
     fi
     for i in $(seq 0 15); do
         lig_path="${dir}/glide_maegz/lig_${i}.maegz"
         if [ ! -f "$lig_path" ]; then
-            echo "No ligand file found in $dir."
+            echo "No ligand file found in $dir"
             continue
         fi
         pv_path="${dir}/lig/lig_${i}.maegz"
 
-        echo "$rec_path and $lig_path to be concatenated into $pv_path."
+        echo "$rec_path and $lig_path to be concatenated into $pv_path"
         "$SCHRODINGER"/utilities/structcat -i "$rec_path" "$lig_path" -o "$pv_path" &
     done
 done
